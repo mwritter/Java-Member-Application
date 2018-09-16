dffc54b9eb1e61892fe68a15bbf826afdf667804
Alexander Amundson <alex.amundson987@gmail.com>
2018-09-16 16:30:52 -0700

    Added a method

--- Membership.java
+++ Membership.java
@@ -1,13 +1,30 @@
 package PackageName;
 
 import java.util.Date;
+import java.util.List;
 
 class Membership {//this class should have package level visibility
 	//instance variables
-	private Date DateJoined;
-	private int points;
+	private Date dateJoined;
+	private int points =0;
+	
+    Member member;
+	Group group;
+	List<Question> questions;
+	List<Answer> answers;
+	
+	
+	
+	//constructor method
+	Membership(Date dateJoined, Member mem, Group grp) {
+		this.dateJoined = dateJoined;
+		this.member = mem;
+		this.group = grp;
+		//this.points = points;
+	}
+	
 	
 	public String toString() {
-		return this +"";
+		return this.member +"joined " + this.group + "on " + this.dateJoined + ".";
 	}
 }
