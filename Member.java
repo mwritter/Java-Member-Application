0d6a21dbc532b3d6843b3a74a183dd73ea6cc4a0
Alexander Amundson <alex.amundson987@gmail.com>
2018-09-16 14:23:08 -0700

    Added some methods to Member class-Still not finished

--- Member.java
+++ Member.java
@@ -1,6 +1,8 @@
 package PackageName;
 
+import java.util.List;
 import java.util.Date;
+import java.util.ArrayList;
 
 public class Member {
 
@@ -11,10 +13,11 @@ public class Member {
 	private String screenName; // emailAddress & screenName must be unique
 	private String emailAddress;
 
-	// Constructor methods
+	private List<Membership> memberships;
 	
-	/*public Member(){}//no-argument constructor
-	 */
+	
+	
+	// Constructor method	
 	public Member(String firstName, String lastName, String screenName, String emailAddress, Date dateCreated) {
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -23,65 +26,147 @@ public class Member {
 		this.dateCreated = dateCreated;
 	}
 
-	// getter methods
+	//start of getter methods
+	//returns email address
 	public String getEmailAddress() {
 		return this.emailAddress;
 	}
 
+	//returns the date member was created
 	public Date getDateCreated() {
 		return this.dateCreated;
 	}
 
+	//returns the member's first name
 	public String getFirstName() {
 		return this.firstName;
 	}
 
+	//returns the member's last name
 	public String getLastName() {
 		return this.lastName;
 	}
 
+	//returns screen name
 	public String getScreenName() {
 		return this.screenName;
 	}
 	// end of getter methods
 
-	public void joinGroup(Group groupName, Date localDateTime) {
-
+	
+    //Joins this member to group and records the dateJoined
+	public void joinGroup(Group groupName, Date LocalDateTime) {
+		Membership mem = new Membership(LocalDateTime, this, groupName);
+	    this.memberships.add(mem);
 	}
 
+	
+	//Returns the number of groups this member is a member of
 	public int getNumGroups() {
-		return ____;
+		return this.memberships.size();
 	}
 
-	public Group getGroup(String _____) {
-		return ____;
+	
+	//Returns the Group that corresponds to the groupID
+	public Group getGroup(String groupID) {
+		Group temp = null;
+		for (Membership M: this.memberships) {
+			if ( M.group.title.equals(groupID) ) {//should group class have group ID variable?
+				temp = M.group;
+			}
+		}
+		return temp;
 	}
 
+	
+	//Returns a list of Groups the member is a member of
 	public List<Group> getGroups() {
-
-		return ____;
+		ArrayList<Group> groupList = new ArrayList<Group>();
+		for (Membership M: this.memberships) {
+			groupList.add(M.group);
+		}
+		return groupList;
 	}
 
+
+	/////!!!!!!!!!!!I am still working on Everything below here. Everything above should be complete!!!!!!!!!!!!!!///////// 
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	
+	
+	
+	
+	
+	//Adds the question to the group by this member and records
+	//the date the question was asked
 	public void addQuestion(Group groupName, Question question, Date date) {
-
+		
+		for (Membership M: memberships) {
+			if (M.group.equals(groupName)) {
+				question.setMembership(M);
+			}
+		}
+				
+				
+			
+		//groupName.getQuestions().add(question);////////////////correct this method//////////////////
 	}
 
-	public Date getDateJoined(Group groupName {
-		return ___;
+	
+	//Returns the date this member joined this group
+	public Date getDateJoined(Group groupName) {
+		Date temp =null;
+		for (Membership M: memberships) {
+			if (M.group.equals(groupName)) {
+				temp=M.group.dateCreated;
+			}
+		}
+		
+		return temp;
 	}
 
+	
+	//Adds this member's answer to this question which is in this group and 
+	//records the date answered
 	public void addAnswer(Group groupName, Question question, Answer answer, Date date) {
+		question.addAnswer(answer);//////
+		
+		//for (Question Q: groupName.getQuestions()) {
+		//	Q.setAnswer(answer);
+		//}
+		
 	}
 
+	
+	//Returns all questions asked by this member in this group
 	public List<Question> getQuestions(Group groupName) {
+		ArrayList<Question> memQuestions = new ArrayList<Question>();
+		for (Membership M: memberships) {
+			memQuestions.addAll( M.questions);
+		}
+		return memQuestions;
 	}
 
+	
+	//Returns all answers asked by this member in this group
 	public List<Answer> getAnswers(Group groupName) {
+		ArrayList<Answer> memAnswers = new ArrayList<Answer>();
+		for (Membership M: memberships) {
+			memAnswers.addAll( M.answers);
+		}
+		return memAnswers;
 	}
 
+	
+	//Provides useful information about this member, neatly formatted
+	@Override
 	public String toString() {
-		return "" + ____;
+		return "Member Information\n" + 
+			   "First Name: " +this.firstName +
+			   "\nLast Name: " + this.lastName + 
+			   "\nScreen Name: " + this.screenName + 
+			   "\nEmail address: " + this.emailAddress + 
+			   "\nDate Joined: " + this.dateCreated;
 
 	}
 
-}
+}//end of Member.java class
