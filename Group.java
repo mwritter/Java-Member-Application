dc600616dd560619a5fc57a8e9ca828f528fcf89
Matthew Ritter <mritter2283@gmail.com>
2018-09-23 23:27:02 -0400

    Wrong file

--- Group.java
+++ Group.java
@@ -1,73 +1,59 @@
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
+import java.util.Date;
+import java.util.ArrayList;
 
-public class Group {
-    Date dateCreated;
-    String title;
-    String description;
-    private List<Answer> answers;
-    private List<Membership> memberships = new ArrayList<>();
-    private List<Question> questions;
+public class MainTest {
 
-    public Group(String title, String description, Date date) {
-        this.title = title;
-        this.description = description;
-        this.dateCreated = date;
-    }
+    public static void main(String[] args) {
 
-    public Date getDateCreated() {
-        return dateCreated;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    // Returns this groups description
-    public String getDescription() {
-        return description;
-    }
-
-    public int getNumberOfMembers() {
-        return memberships.size();
-    }
-
-    // Returns the Member of this group that corresponds to the emailAddress
-    public Member getMember(String emailAddress) {
-
-        for (Membership membership : memberships) {
-            if (membership.member.getEmailAddress().equals(emailAddress)) {
-                return membership.member;
-            }
-        }
         Date date = new Date();
-        return new Member("Dummy", "Dummy", "NoScreenName", "NoEmail", date);
-    }
 
-    public List<Member> getMembers() {
-        List<Member> members = new ArrayList<>();
-        for (Membership membership : memberships) {
-            members.add(membership.member);
+        Member matt = new Member("Matthew", "Ritter", "Matt", "mwritter@valdosta.edu", date);
+        Member virginia = new Member("Virginia", "Ritter", "Gin", "vrharnevious@valdosta.edu", date);
+
+        Group group1 = new Group("First Group", "This is an awesome Group1", date);
+        Group group2 = new Group("Second Group", "This is an awesome Group2", date);
+
+        matt.joinGroup(group1, date);
+        matt.joinGroup(group2, date);
+        virginia.joinGroup(group2, date);
+
+        Question question1 = new Question("First Question Title", "Is this a question?", date);
+        Question question2 = new Question("Second Question Title", "Do you know?", date);
+
+        matt.addQuestion(group1, question1, date);
+        matt.addQuestion(group1, question2, date);
+        matt.addQuestion(group2, question2, date);
+
+        System.out.println("============");
+        System.out.println("getMember(mwritter@valdosta.edu):" + group1.getMember("mwritter@valdosta.edu"));
+        System.out.println("============");
+
+        for (Group group : matt.getGroups()) {
+            System.out.println(matt.getScreenName() + " Joined: " + group.getTitle() + " " + matt.getDateJoined(group)
+                    + "\n" + "============");
         }
-        return members;
+        for (Group group : virginia.getGroups()) {
+            System.out.println(virginia.getScreenName() + " Joined: " + group.getTitle() + " "
+                    + virginia.getDateJoined(group) + "\n" + "============\n");
+        }
+        System.out.println("============");
+        System.out.println("getMembers(group2):" + group2.getMembers());
+        System.out.println("============");
+        Answer answer1 = new Answer(question1, "This is the answer", date);
+
+        matt.addAnswer(group1, question1, answer1, date);
+        System.out.println("============");
+        System.out.println("GetQuestions For Matt: \n" + matt.getQuestions(group1));
+        System.out.println("============");
+        System.out.println("GetAnswers For Matt: " + matt.getAnswers(group1));
+        System.out.println("============");
+        System.out.println("GetAuthor of answer1" + answer1.getAuthor());
+        System.out.println("============");
+        System.out.println("Member toString method: " + matt.toString());
+        System.out.println("============");
+        System.out.println("No errors");
+
     }
 
-    // Returns all questions that have been asked in this group in the order
-    // that they were asked
-    public List<Question> getQuestions() {
-        return questions;
-    }
-
-    // Returns all answers to all questions that have been asked in this group in
-    // the order
-    // that they were answered
-    public List<Answer> getAnswers() {
-        return answers;
-    }
-
-    // Provides useful information about this group, neatly formatted
-    public String toString() {
-        return "Group: " + this.title;
-    }
 }
