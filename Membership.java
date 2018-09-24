2be98675cbb7583379e9bb473f5021dde9c409d0
Alexander Amundson <alex.amundson987@gmail.com>
2018-09-24 15:24:12 -0700

    final changes

--- Membership.java
+++ Membership.java
@@ -1,29 +1,16 @@
-package PackageName;
-
-
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
-class Membership {//this class should have package level visibility
+class Membership {
 	//instance variables
 	private Date dateJoined;
-	private int points =0;
-	
-	
+	private int points =0;	
     private Member member;
 	private Group group;
 	private List<Question> questions = new ArrayList<>();
 	private List<Answer> answers = new ArrayList<>();
 	
-	
-	
-	void addQuestion(Question question) {
-		this.questions.add(question);
-	}
-	void addAnswer(Answer answer) {
-		this.answers.add(answer);
-	}
 
 	//constructor method
 	public Membership(Date dateJoined, Member member, Group group) {
@@ -31,7 +18,8 @@ class Membership {//this class should have package level visibility
 		this.member = member;
 		this.group = group;
 		//this.points = points; 
-	}
+	}	
+	
 	
 	//returns the date the member joined the group
     Date getDateJoined() {
@@ -57,14 +45,15 @@ class Membership {//this class should have package level visibility
 	}
 
 
-  //returns all answers in this membership
+    //returns all answers in this membership
 	List<Answer> getAnswers() {
 		return answers;
 	}
 	
-	
+
     //returns information about member and group and the date the member joined
 	public String toString() {
 		return "\n" + this.member +"joined " + this.group + "on " + this.dateJoined + ".";
 	}
+	
 }
