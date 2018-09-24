dab8fcb2d90c7a03af3601726bfdee1ae6d835b4
Alexander Amundson <alex.amundson987@gmail.com>
2018-09-23 18:34:16 -0700

    deleted unnecessary package name

--- Membership.java
+++ Membership.java
@@ -1,5 +1,3 @@
-package PackageName;
-
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
@@ -8,12 +6,10 @@ class Membership {//this class should have package level visibility
 	//instance variables
 	private Date dateJoined;
 	private int points =0;
-	
     Member member;
 	Group group;
 	List<Question> questions = new ArrayList<>();
 	List<Answer> answers = new ArrayList<>();
-	
 	
 	
 	//constructor method
@@ -21,17 +17,18 @@ class Membership {//this class should have package level visibility
 		this.dateJoined = dateJoined;
 		this.member = member;
 		this.group = group;
-		//this.points = points;
+		//this.points = points; 
 	}
 	
 	
 	//returns the date the member joined the goup
-    Date getDateJoined() {//package-level visibility
+    Date getDateJoined() {
 		return this.dateJoined;
 	}
 	
 	
+    //returns information about member and group and the date the member joined
 	public String toString() {
-		return this.member +"joined " + this.group + "on " + this.dateJoined + ".";
+		return "\n" + this.member +"joined " + this.group + "on " + this.dateJoined + ".";
 	}
 }
