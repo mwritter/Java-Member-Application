4c062a1638c5b0179d7497970bfe5692984d8c66
Matthew Ritter <mritter2283@gmail.com>
2018-10-13 21:44:09 -0400

    UPDATED file structure to better use eclipse

---
+++ Sprint1/src/sprint1/GroupTest.java
@@ -0,0 +1,64 @@
+package sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GroupTest {
+
+	@Test
+	void testGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDateCreated() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTitle() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetNumberOfMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMember() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddMembership() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMembers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetQuestions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAnswers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
