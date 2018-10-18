6f777b3a9787253aa2dcdcca23893d70bc7775bf
Matthew Ritter <mritter2283@gmail.com>
2018-10-17 23:07:32 -0400

    CREATED AnswerTest JUnit test, and ADDED testAnswer

---
+++ Sprint1/src/sprint1/AnswerTest.java
@@ -0,0 +1,33 @@
+package sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AnswerTest {
+
+	Date date = new Date();
+	Question q = new Question("Question One", "This is a question?", date);
+	Answer a = new Answer(q, "This is an answer!", date);
+	
+	
+	@Test
+	void testAnswer() {
+		String expected = "Answer: This is an answer! for question Question One";
+		assertTrue(a.toString().equals(expected));
+	}
+
+	@Test
+	void testGetQuestion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
