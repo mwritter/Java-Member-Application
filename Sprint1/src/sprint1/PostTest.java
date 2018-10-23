89b575f3b34c2a088c52b0dff469a53d9ca1de2c
Matthew Ritter <mritter2283@gmail.com>
2018-10-23 17:47:26 -0400

    ADDED PostTest JUnit test, CREATED getText, setText, and getAuthor test

---
+++ Sprint1/src/sprint1/PostTest.java
@@ -0,0 +1,90 @@
+package sprint1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+
+class PostTest {
+
+	@Test
+	void testPost() {
+		Date date = new Date();
+		Member matt = new Member("Matthew", "Ritter", "Matt", "mritter2283@gmail.com", date);
+		Group group1 = new Group("Group One", "The best Group ONE", date);
+		matt.joinGroup(group1, date);
+		Question q1 = new Question("Q1 Title", "Whats the weather like", date);
+		Answer a1 = new Answer(q1, "Weather looks greate", date);
+		matt.addQuestion(group1, q1, date);
+		
+		
+	}
+
+	@Test
+	void testGetText() {
+		Date date = new Date();
+		Member matt = new Member("Matthew", "Ritter", "Matt", "mritter2283@gmail.com", date);
+		Group group1 = new Group("Group One", "The best Group ONE", date);
+		matt.joinGroup(group1, date);
+		Question q1 = new Question("Q1 Title", "Whats the weather like", date);
+		Answer a1 = new Answer(q1, "Weather looks greate", date);
+		matt.addQuestion(group1, q1, date);
+		matt.addAnswer(group1, q1, a1, date);
+		
+		
+		
+		assertEquals("Weather looks greate", a1.getText());
+	}
+
+	@Test
+	void testGetDate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetText() {
+		Date date = new Date();
+		Member matt = new Member("Matthew", "Ritter", "Matt", "mritter2283@gmail.com", date);
+		Group group1 = new Group("Group One", "The best Group ONE", date);
+		matt.joinGroup(group1, date);
+		Question q1 = new Question("Q1 Title", "Whats the weather like", date);
+		Answer a1 = new Answer(q1, "Weather looks greate", date);
+		matt.addQuestion(group1, q1, date);
+		matt.addAnswer(group1, q1, a1, date);
+		
+		a1.setText("I reset the answer");
+		
+		assertEquals("I reset the answer", a1.getText());
+	}
+
+	@Test
+	void testGetAuthor() {
+		Date date = new Date();
+		Member matt = new Member("Matthew", "Ritter", "Matt", "mritter2283@gmail.com", date);
+		Group group1 = new Group("Group One", "The best Group ONE", date);
+		matt.joinGroup(group1, date);
+		Question q1 = new Question("Q1 Title", "Whats the weather like", date);
+		Answer a1 = new Answer(q1, "Weather looks greate", date);
+		matt.addQuestion(group1, q1, date);
+		matt.addAnswer(group1, q1, a1, date);
+		
+		assertEquals(matt, a1.getAuthor());
+	}
+
+	@Test
+	void testGetGroup() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMembership() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMembership() {
+		fail("Not yet implemented");
+	}
+
+}
