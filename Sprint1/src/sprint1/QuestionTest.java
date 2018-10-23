e5a483936b1369ab2c297e5ea1133e98ed8b4a90
Matthew Ritter <mritter2283@gmail.com>
2018-10-23 18:12:00 -0400

    ADDED addAnswer test to QuestionTest

--- Sprint1/src/sprint1/QuestionTest.java
+++ Sprint1/src/sprint1/QuestionTest.java
@@ -34,7 +34,16 @@ class QuestionTest {
 
 	@Test
 	void testAddAnswer() {
-		fail("Not yet implemented");
+		Date date = new Date();
+		Member matt = new Member("Matthew", "Ritter", "Matt", "mritter2283@gmail.com", date);
+		Group group1 = new Group("Group One", "The best Group ONE", date);
+		matt.joinGroup(group1, date);
+		Question q1 = new Question("Q1 Title", "Whats the weather like", date);
+		Answer a1 = new Answer(q1, "Weather looks greate", date);
+		matt.addQuestion(group1, q1, date);
+		matt.addAnswer(group1, q1, a1, date);
+		
+		assertTrue(q1.getAnswers().contains(a1));
 	}
 
 	@Test
