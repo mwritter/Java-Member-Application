bc4dad6285a94d72289fbc66bce5fb01ed4536ed
Matthew Ritter <mritter2283@gmail.com>
2018-10-26 18:57:45 -0400

    FIXED persistance manager constructor syntax, and return SiteManager from read method

--- Sprint1/src/sprint1/PersistanceManager.java
+++ Sprint1/src/sprint1/PersistanceManager.java
@@ -4,14 +4,15 @@ import java.io.ObjectOutputStream;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 
 class PersistanceManager {
    
-   public PersistanceManager {
+   public PersistanceManager() {
    }
    
    //Static method to save the entire system. Hint: a binary file is the easiest way to do this
-   public static save (SiteManager sm, FileOutputStream file) {
+   public static void save(SiteManager sm, FileOutputStream file) {
       try {
          ObjectOutputStream binFile = new ObjectOutputStream(file);
          binFile.writeObject(sm);
@@ -25,8 +26,10 @@ class PersistanceManager {
    }
    
    //Static method to read the system from file and return a SiteManager object
-   public static read (FileInputStream file) {
+   public static SiteManager read (FileInputStream file) {
       ObjectInputStream binFile = new ObjectInputStream(file);
-      SiteManager savedPersistanceManager = (SiteManager) binFile.readObject();
+      SiteManager savedSiteManager = (SiteManager) binFile.readObject();
+      
+      return savedSiteManager;
    }
 }
